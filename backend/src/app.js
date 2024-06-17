e726c9de53c3b22d23661b19c8c7cefabd135894
Marco <marcodavidtoledo@gmail.com>
2024-06-16 20:00:54 -0400

    feat:cors solutino

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://si1-bus.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
