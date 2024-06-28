18953e4085c7cde94b3b8d1ccfba39597a6df592
Marco <marcodavidtoledo@gmail.com>
2024-06-28 06:32:29 -0400

    feat:correction fecha hora login

--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -75,5 +75,5 @@ export const PrivateRoutes = {
 };
 
 //url backend
-export const URL_BACKEND = "http://localhost:4000/api"
-/* export const URL_BACKEND = "https://si1-bus-back.onrender.com/api"; */
+/* export const URL_BACKEND = "http://localhost:4000/api" */
+export const URL_BACKEND = "https://si1-bus-back.onrender.com/api";
